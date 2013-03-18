da3c2fa54d8ab34749e7d84286db3b23c2288849
David Lau <bbc093@hotmail.com>
2013-03-18 19:43:29 -0400

    push today

--- js/script.js
+++ js/script.js
@@ -4,12 +4,55 @@ $(document).ready(function(){
 	});
 });
 
+$(document).ready(function(){
+	$(".mini_item").hover(function(){
+		$(this).find(".CTA_button_mini").toggle();
+	});
+});
+
 $(document).ready(function() {
 	$(".item_tab").click(function() {
 		$(this).parent().siblings(".item_content").slideToggle();
 	});
 });
 
+$(document).ready(function() {
+	$(".item_tab").click(function(){
+		$(this).siblings(".item_content").slideToggle();
+	});
+});
+
 // $(document).ready(function(){
 // 	$("#Outing_frame").resizable();
 // });
+
+$(function() {
+       var datePicker = $('#datepicker').datepicker({
+ 		 	inline: true,  
+        	showOtherMonths: true,  
+        	dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+       });
+
+    $(".demo").scroll(function() {
+      datePicker.datepicker('hide');
+      $('#datepicker').blur();  
+    });
+    
+    $(window).resize(function() {
+      datePicker.datepicker('hide');
+            $('#datepicker').blur();  
+    });
+});
+
+$(document).ready(function() { 
+	$("#e1").select2(); 
+});
+
+$(document).ready(function(){
+	$(".btn-destory").click(function(){
+		$(this).closest(".content_btn").fadeOut("slow",function(){
+			$(this).remove();
+		});
+	});
+});
+

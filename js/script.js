3961427beec19ee62d039a98de9c7b8841ee94c2
David Lau <bbc093@hotmail.com>
2013-06-28 18:13:00 -0400

    New story page

--- js/script.js
+++ js/script.js
@@ -1,67 +1,19 @@
-// $(document).ready(function(){
-// 	$(".event_item").hover(function(){
-// 		$(this).find(".CTA_button").toggle();
-// 	});
-// });
-
-// $(document).ready(function(){
-// 	$(".nearby_item").hover(function(){
-// 		$(this).find(".CTA_button").toggle();
-// 	});
-// });
-
-// $(document).ready(function(){
-// 	$(".mini_item").hover(function(){
-// 		$(this).find(".CTA_button_mini").toggle();
-// 	});
-// });
-
-$(document).ready(function() {
-	$(".item_tab").click(function() {
-		$(this).parent().siblings(".item_content").slideToggle();
-		$(this).children("span").toggleClass("up_arrow");
-	});
-});
-
-$(document).ready(function() {
-	$(".item_tab").click(function(){
-		$(this).siblings(".item_content").slideToggle();
-	});
-});
-
-// $(document).ready(function(){
-// 	$("#Outing_frame").resizable();
-// });
-
-$(function() {
-       var datePicker = $('#datepicker').datepicker({
- 		 	inline: true,  
-        	showOtherMonths: true,  
-        	dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-       });
-
-    $(".demo").scroll(function() { 
-      datePicker.datepicker('hide');
-      $('#datepicker').blur();  
-    });
-    
-    $(window).resize(function() {
-      datePicker.datepicker('hide');
-            $('#datepicker').blur();  
-    });
-});
 
 $(document).ready(function(){
-	$(".btn-destory").click(function(){
-		$(this).closest(".content_btn").fadeOut("slow",function(){
-			$(this).remove();
-		});
+	$(".mini_item").hover(function(){
+		$(this).find(".CTA_button_mini").toggle();
 	});
+		$(".item_tab").click(function() {
+			$(this).parent().children(".item_content").slideToggle();
+			$(this).children("span").toggleClass("icon-chevron-down");
+			$(this).children("span").toggleClass("icon-chevron-up");
+//			$(this).children("span").toggleClass("up_arrow");
+		});
+	$(".nearby_item").off('mouseenter mouseleave');
+	$(".nearby_item").hover(function(){
+		$(this).find(".CTA_button").toggle();
+	});
+
 });
 
 
-
-      $(function(){ 
-        $(".tier3").popover({container:'body',});  
-      });  
-         
